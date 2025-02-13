1548d6177d45718e821d1ca359feb5585b8749da
mahmoud ibrahim <mibrahim@localhost.localdomain>
2025-02-13 02:56:16 +0200

    Java Web Application for Telecom SMS Service

---
+++ TwilioSMSClient/src/main/java/com/iti/project/TwilioSMSClient/model/SMS.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.iti.project.TwilioSMSClient.model;
+
+/**
+ *
+ * @author mibrahim
+ */
+public class SMS {
+    
+}
